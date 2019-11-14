83fb80d2b3f62369bd712e962a4db18622c4df7a
Bruce Kim <bruce.kim@buzzvil.com>
2019-11-14 15:51:16 +0900

    Add sessionReadyReceiver

--- app/src/main/java/com/buzzvil/buzzscreen/popsample/java/MainActivity.java
+++ app/src/main/java/com/buzzvil/buzzscreen/popsample/java/MainActivity.java
@@ -1,14 +1,35 @@
 package com.buzzvil.buzzscreen.popsample.java;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.buzzvil.buzzad.benefit.BuzzAdBenefit;
 import com.buzzvil.buzzscreen.popsample.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver sessionReadyReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("SessionKey", "Session is Ready. Ads can be loaded now.");
+            Toast.makeText(MainActivity.this, "Session is Ready. Ads can be loaded now.", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BuzzAdBenefit.registerSessionReadyBroadcastReceiver(MainActivity.this, sessionReadyReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        BuzzAdBenefit.unregisterSessionReadyBroadcastReceiver(this, sessionReadyReceiver);
     }
 }
